cba4f841271567311c35ed7797cc9bbaf403c61b
Kineruu <Kineru08@gmail.com>
2026-04-15 18:04:19 +0200

    Added the window opening up when the hotkey is pressed.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import customtkinter as ct
 import keyboard
+import threading
+import webbrowser
 import time
-
 """
 TODO:
 -> Make it check for each keyboard letter?
@@ -9,12 +11,42 @@ gh -> github, yt -> youtube, yt something -> searches for something in youtube e
 -> Maybe open up a cmd window who knows
 """
 
-keys = input("KEYBIND: ")
-if keys == "": keys = "ctrl+shift+c" # Default hotkey for now
+window = ct.CTk()
+window.geometry("100x50")
+window.resizable(False, False)
+frame = ct.CTkFrame(master=window)
 
-def keyboard_listener():
-    print("HOTKEY DETECTED")
+entry_input = ct.CTkEntry(window, width=95, height=45)
+entry_input.pack()
 
-keyboard.add_hotkey(keys, keyboard_listener)
-print("Listening for keys, press ESC to quit.")
-keyboard.wait("esc")
+window.withdraw()
+
+#keys = input("KEYBIND: ")
+#if keys == "": keys = "ctrl+shift+c" # Default hotkey for now
+keys = "ctrl+shift+c"
+
+def show_window():
+    window.deiconify()
+    window.lift()
+    window.focus_force()
+    entry_input.focus()
+
+def keyboard_listener(event=None):
+    text = entry_input.get()
+    print(f"HOTKEY DETECTED {text}")
+
+    if text == "gh":
+        webbrowser.open("https://github.com")
+        window.withdraw()
+    else:
+        window.withdraw()
+    
+entry_input.bind("<Return>", keyboard_listener)
+
+def start_listening():
+    keyboard.add_hotkey(keys, show_window)
+    keyboard.wait()
+
+threading.Thread(target=start_listening, daemon=True).start()
+
+window.mainloop()
